78f7ea6170bfb4f8adc3c6f7caa12dc9d0d772dd
Manuel <72426487+MAGE-VOID@users.noreply.github.com>
2025-02-13 16:25:08 -0500

    .

--- V2/main_backtest.py
+++ V2/main_backtest.py
@@ -1,3 +1,4 @@
+import cProfile
 import pandas as pd
 from datetime import datetime
 import MetaTrader5 as mt5
@@ -6,16 +7,15 @@ import Backtest_Engine as BT
 import visualize as VZ
 import StrategySignal as Signal
 
-
-# Connect to trading account specifying the Number, Server and Password
+# Conectar a la cuenta de trading
 CL.connect_and_login_mt5(51344621, "ICMarketsSC-Demo", "bCFNLB9k")
 
-
-# Uso de la función
+# Definir fechas de inicio y fin
 inp_start_date = datetime(2010, 1, 1)
 inp_end_date = datetime(2024, 12, 31)
 timeframe = mt5.TIMEFRAME_M5  # M1, M5, M10, M15, M30, H1, H4, D1, W1
-# Definir símbolos
+
+# Definir los símbolos
 symbols = [
     "AUDNZD",
     "AUDUSD",
@@ -28,24 +28,36 @@ symbols = [
     "USDCAD",
 ]
 
+# Descargar y procesar los datos
 df, df_standardized = CL.process_data(inp_start_date, inp_end_date, symbols, timeframe)
 
+# Crear instancia del motor de backtest
 bt_engine = BT.BacktestEngine(initial_balance=1000)
+
+# Usar cProfile para medir el rendimiento del backtest
+profiler = cProfile.Profile()
+profiler.enable()
+
+# Ejecutar el backtest
 resultBacktest = bt_engine.run_backtest(
     df, strategy_name="grid_buy", strategy_signal_class=Signal.StrategySignal
 )
 
-# Obtener las estadísticas del backtest
+profiler.disable()
+
+# Guardar el resultado del perfilado en un archivo
+profiler.dump_stats("backtest_profile.prof")
+
+# Obtener y mostrar las estadísticas del backtest
 statistics = resultBacktest["statistics"]
 stats_df = pd.DataFrame.from_dict(statistics, orient="index", columns=["Value"])
 stats_df["Value"] = stats_df["Value"].apply(
     lambda x: f"{x:,.2f}" if isinstance(x, (int, float)) else x
 )
+
+# Mostrar las estadísticas
 print("\n--- Backtest Statistics ---")
 print(stats_df)
 
-# Llamar a la función para graficar equity y balance
+# Graficar el balance y la equidad
 VZ.plot_equity_balance(resultBacktest)
-
-# Graficar las señales para un símbolo específico
-# VZ.plot_price_with_signals(resultBacktest, df, "EURUSD")
