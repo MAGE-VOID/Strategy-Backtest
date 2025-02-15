e5bb6f7a262917585e88f95a3f3eed781f825ecd
Manuel <72426487+MAGE-VOID@users.noreply.github.com>
2025-02-14 20:37:18 -0500

    .

--- V4/Extract.py
+++ V4/Extract.py
@@ -9,7 +9,11 @@ def mostrar_estructura(ruta, prefijo="", salida=""):
     """Muestra la estructura de directorios similar al comando tree y devuelve la cadena."""
     try:
         # Excluir el script actual y cualquier __pycache__
-        elementos = [e for e in os.listdir(ruta) if e not in {os.path.basename(__file__), "__pycache__"}]
+        elementos = [
+            e
+            for e in os.listdir(ruta)
+            if e not in {os.path.basename(__file__), "__pycache__"}
+        ]
         for i, elemento in enumerate(elementos):
             ruta_elemento = os.path.join(ruta, elemento)
             es_ultimo = i == len(elementos) - 1
@@ -30,7 +34,6 @@ def mostrar_estructura(ruta, prefijo="", salida=""):
         return f"Error: Permiso denegado para acceder a: {ruta}\n"
     except Exception as e:
         return f"Error al listar el directorio {ruta}: {e}\n"
-
 
 
 def procesar_archivos(ruta_base):
@@ -59,7 +62,6 @@ def procesar_archivos(ruta_base):
     except Exception as e:
         print(f"Error al procesar la carpeta: {e}")
     return contenido_total
-
 
 
 if __name__ == "__main__":
