1b3acb83a24d507f6739aa98c43fe402ce53b116
Manuel <72426487+MAGE-VOID@users.noreply.github.com>
2025-02-25 18:35:11 -0500

    .

--- V8/backtest/optimization_engine.py
+++ V8/backtest/optimization_engine.py
@@ -10,6 +10,7 @@ import time
 from backtest.stats import Statistics
 from backtest.managers.entry_manager import EntryManager
 from backtest.utils.barprogress_multi import MultiProgress
+from backtest.algorithm.parameter_combinations import generate_combinations
 
 
 class OptimizationEngine:
@@ -24,43 +25,12 @@ class OptimizationEngine:
         self.best_stats = None
         self.equity_over_time = []
 
-    def generate_combinations(self, optimization_params):
-        param_values = []
-        for i, (param_key, param_info) in enumerate(
-            optimization_params.items(), start=1
-        ):
-            param_type = param_info.get("type", "float")
-            if param_type in ["int", "float"]:
-                start = param_info.get("start")
-                stop = param_info.get("stop")
-                step = param_info.get("step")
-                if start is None or stop is None or step is None:
-                    raise ValueError(
-                        f"Parámetros de rango no definidos correctamente para {param_key}"
-                    )
-                generated_values = np.arange(
-                    start, stop, step, dtype=float if param_type == "float" else int
-                )
-                print(
-                    f"Lista {i} para {param_key} (tipo {param_type}): {generated_values}"
-                )
-                param_values.append(generated_values)
-            elif param_type == "bool":
-                generated_values = [True, False]
-                print(f"Lista {i} para {param_key} (tipo bool): {generated_values}")
-                param_values.append(generated_values)
-            else:
-                raise ValueError(
-                    f"Tipo de parámetro desconocido para {param_key}: {param_type}"
-                )
-        return list(product(*param_values))
-
     def run_optimization(self):
         # Instanciar la señal para obtener los parámetros de optimización
         strategy_signal = self.config.strategy_signal_class(self.input_data)
         optimization_params = strategy_signal.optimization_params
 
-        param_combinations = self.generate_combinations(optimization_params)
+        param_combinations = generate_combinations(optimization_params)
         total_combinations = len(param_combinations)
         print(f"\nTotal de combinaciones de parámetros: {total_combinations}\n")
 
